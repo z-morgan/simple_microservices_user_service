545cf7072e9f3d75078b809a808b8ef3289b96de
Zach Morgan <zachmorgan97@gmail.com>
2023-06-10 12:56:04 -0700

    initial express app

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+	res.send('Hello from users_service');
+});
+
+app.get('/users/:userId', (req, res) => {
+  const users = [
+    {
+      userId: 1,
+      username: 'mimmy',
+      touchedBy: [
+        "user_service",
+      ],
+    },
+  ];
+
+  const user = users.find(user => user.userId === Number(req.params.userId));
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(user));
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
